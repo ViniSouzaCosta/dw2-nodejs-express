9da2830baebeb770116703cc9c2c9c7f7d87b930
ViniSouzaCosta <vinicius.souzacamargocosta@gmail.com>
2025-02-13 18:45:08 -0300

    Variáveis e funções no JS

---
+++ aula-0.1-variaveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,22 @@
+//Comentário em linha
+/* Comentário
+em bloco ... */
+// Vinicius CRTL + ;
+
+// Declaração de variaáveis no JS.
+// CONST x LET X VAR.
+// Por questões de segurança evitar o uso de VAR.
+// No escopo global CONST não pode ser alterado.
+
+const nome = "Vinícius" // Uma constante precisa de um valor.
+let Nome // Permite iniciar a variável vazia.
+
+// const nome = "Pedro" -> Resulta em um erro.
+// let Nome -> Resulta em um erro.
+var cor = "azul"
+var cor = "amarelo"
+let cidade = "Registro"
+cidade = "Jacupiranga" // Let permite alterar o valor.
+
+const message = "Hello world! iniciando estudos com Javascript"
+console.log(message)
