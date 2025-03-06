61dca1f5f6b1dea81c8b1c3fdbbfcfaceb73ec86
ViniSouzaCosta <vinicius.souzacamargocosta@gmail.com>
2025-03-06 19:23:09 -0300

    @ViniSouzaCosta

---
+++ aula-0.2-arrays-e-objetos/js-aula-0.2-arrays-e-objetos.js
@@ -0,0 +1,27 @@
+//  ARRAY (Vetor ou Lista)
+let produtos = []
+console.log(typeof(produtos))
+let Produtos = ["Computador", "Notebook","Celular", "Tablet"]
+    console.log(Produtos)
+
+    console.log("Exibindo a lista pelos índices...")
+    console.log(`1- ${Produtos[0]}`)
+    console.log(`2- ${Produtos[1]}`)
+    console.log(`3- ${Produtos[2]}`)
+    console.log(`4- ${Produtos[3]}`)
+
+    console.log("exibindo a lista através do FOR...")
+    for (let c in Produtos){
+        console.log(`${Number(c) + 1} - ${Produtos[c]}`)
+    }
+
+
+    console.log("Exibindo a lista através do forEach...")
+    Produtos.forEach(function (produto) {
+    console.log(produto)
+    })
+
+    console.log("Exibindo a lista através do forEach com os índices...")
+    Produtos.forEach((produto, i) => {
+    console.log(`${i+1} - ${produto}`)
+    })
